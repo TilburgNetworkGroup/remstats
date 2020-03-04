4acaa0edc9f4ce151e6e51b15801484eb472ce7d
meij-m <57525126+meij-m@users.noreply.github.com>
2020-03-04 13:39:46 +0100

    Create covar example data

---
+++ data-raw/covariates.R
@@ -0,0 +1,30 @@
+rm(list = ls())
+
+set.seed(54688)
+
+# Time-varying covariate information
+# Set up the data-frame
+covar <- data.frame(id = rep(1:26, each = 3), 
+    time = rep(c(0, 5, 10), times = 26),
+    x1 = round(runif(26*3, -2, 2), 1), 
+    x2 = round(rnorm(26*3, 0, 2)))
+
+# Sample measurements
+covar <- covar[-sort(sample(c(seq(2, nrow(covar), 3), 
+    seq(3, nrow(covar), 3)), 1.2*26)),]
+
+# Add randomness to time
+covar$time <- ifelse(covar$time != 0, 
+    covar$time+runif(nrow(covar), min = -1, max = 1), 
+	covar$time)
+
+# Tidy up dataframe
+covar$time <- round(covar$time, 2)
+rownames(covar) <- NULL
+names(covar) <- c("id", "change_time", "x1", "x2")
+covar <- as.matrix(covar)
+
+usethis::use_data(covar)
+
+
+
