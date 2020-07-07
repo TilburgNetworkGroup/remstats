39d100cfb2f8724348580b8cd3cb6aeb112f81a1
meij-m <57525126+meij-m@users.noreply.github.com>
2020-07-07 08:53:27 +0200

    Fix bug in compute_triad

--- tests/testthat/test_undirected_triad_stats.R
+++ tests/testthat/test_undirected_triad_stats.R
@@ -7,11 +7,49 @@ n <- length(unique(info$id))
 test_that("scaling is counts", {
 	# Compute the stats
 	out <- remstats(~ sp() + spUnique(), edgelist = history, directed = FALSE)
+	edgelist <- out$edgelist
+	riskset <- out$riskset
 	stats <- out$statistics
 	
 	# Tests
 	expect_true(all(stats[,,"sp"] >= stats[,,"spUnique"]))
 	expect_true(max(stats[,,"spUnique"]) <= n)
+	
+	statrow1 <- apply(riskset, 1, function(x) {
+		min1 <- edgelist[-nrow(edgelist),]
+		
+		sub1 <- min1[min1[,2] == x[1] | min1[,3] == x[1],]
+		sub2 <- min1[min1[,2] == x[2] | min1[,3] == x[2],]
+		
+		p1 <- c(sub1[,2], sub1[,3])
+		p1 <- p1[p1!=x[1] & p1!=x[2]]
+		p2 <- c(sub2[,2], sub2[,3])
+		p2 <- p2[p2!=x[1] & p2!=x[2]]
+		
+		sum(unlist(sapply(unique(p1), function(y) {
+			if(y %in% p2) {
+				min(c(sum(p1 == y), sum(p2 == y)))
+			}
+		})))
+	})
+	
+	expect_equal(stats[nrow(stats),,"sp"], statrow1)
+	
+	statrow2 <- apply(riskset, 1, function(x) {
+		min1 <- edgelist[-nrow(edgelist),]
+		
+		sub1 <- min1[min1[,2] == x[1] | min1[,3] == x[1],]
+		sub2 <- min1[min1[,2] == x[2] | min1[,3] == x[2],]
+		
+		p1 <- unique(c(sub1[,2], sub1[,3]))
+		p1 <- p1[p1!=x[1] & p1!=x[2]]
+		p2 <- unique(c(sub2[,2], sub2[,3]))
+		p2 <- p2[p2!=x[1] & p2!=x[2]]
+		
+		sum(p1 %in% p2)
+	})
+	
+	expect_equal(stats[nrow(stats),,"spUnique"], statrow2)
 })
 
 test_that("scaling is standardize", {
